41cf727432008f9ffc35746281b1b2a6cf3d463c
Noah Bystrom <noah.bystrom@calbt.com>
2018-07-16 14:40:40 -0700

    Make a mixin from sorting functions

---
+++ src/components/sort.js
@@ -0,0 +1,58 @@
+export const sort = {
+  methods: {
+    Sorted (array, firstSort, secondSort) {
+      var firstSortObject = {}
+      for (var i = 0; i < array.length; i++) {
+        firstSortObject[array[i][firstSort]] = []
+      }
+      for (i = 0; i < array.length; i++) {
+        firstSortObject[array[i][firstSort]].push(array[i])
+      }
+      var listOfFirst = []
+      var sorted = []
+      for (var k in firstSortObject) { listOfFirst.push(k) }
+      listOfFirst = this.Sort(listOfFirst)
+      for (var j = 0; j < listOfFirst.length; j++) {
+        // console.log(firstSortObject[listOfFirst[j]])
+        sorted.push.apply(sorted, this.SortByAttribute(firstSortObject[listOfFirst[j]], secondSort))
+      }
+      // console.log(sorted)
+      return sorted
+    },
+
+    // very simple insertion sort O(n^2)
+    // chosen for simplicity and because this is a very small data set
+    // with very large data sets, it may be better to replace one or both of the search algorithms with merge- or quicksort
+    Sort (arr) {
+      console.log(arr)
+      for (var i = 1; i < arr.length; i++) {
+        var value = arr[i]
+        var test = i - 1
+        while (test > -1 && arr[test] > value) {
+          arr[test + 1] = arr[test]
+          test = test - 1
+        }
+        arr[test + 1] = value
+      }
+      console.log(arr)
+      return arr
+    },
+
+    // the same insertion sort, by sub-property "name" this time
+    SortByAttribute (arr, attribute) {
+      console.log(arr)
+      for (var i = 0; i < arr.length; i++) {
+        var value = arr[i][attribute]
+        var test = i - 1
+        while (test > -1 && arr[test][attribute] > value) {
+          arr[test + 1][attribute] = arr[test][attribute]
+          test -= 1
+        }
+        arr[test + 1][attribute] = value
+      }
+      console.log(arr)
+      return arr
+    },
+
+  }
+}
