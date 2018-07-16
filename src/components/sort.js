281f89cd27b48c42e59e11bb589d1e572ba283a7
Noah Bystrom <noah.bystrom@calbt.com>
2018-07-16 15:30:48 -0700

    Enable optional reverse sort

--- src/components/sort.js
+++ src/components/sort.js
@@ -1,6 +1,7 @@
 export const sort = {
   methods: {
-    DeepSort (array, firstSort, secondSort) {
+    DeepSort (array, firstSort, secondSort, reverse) {
+      reverse = reverse || false
       var firstSortObject = {}
       for (var i = 0; i < array.length; i++) {
         firstSortObject[array[i][firstSort]] = []
@@ -11,10 +12,10 @@ export const sort = {
       var listOfFirst = []
       var sorted = []
       for (var k in firstSortObject) { listOfFirst.push(k) }
-      listOfFirst = this.Sort(listOfFirst)
+      listOfFirst = this.Sort(listOfFirst, reverse)
       for (var j = 0; j < listOfFirst.length; j++) {
         // console.log(firstSortObject[listOfFirst[j]])
-        sorted.push.apply(sorted, this.SortByAttribute(firstSortObject[listOfFirst[j]], secondSort))
+        sorted.push.apply(sorted, this.SortByAttribute(firstSortObject[listOfFirst[j]], secondSort, reverse))
       }
       // console.log(sorted)
       return sorted
@@ -23,14 +24,23 @@ export const sort = {
     // very simple insertion sort O(n^2)
     // chosen for simplicity and because this is a very small data set
     // with very large data sets, it may be better to replace one or both of the search algorithms with merge- or quicksort
-    Sort (arr) {
+    Sort (arr, reverse) {
+      reverse = reverse || false
       console.log(arr)
       for (var i = 1; i < arr.length; i++) {
         var value = arr[i]
         var test = i - 1
-        while (test > -1 && arr[test] > value) {
-          arr[test + 1] = arr[test]
-          test = test - 1
+        if (reverse) {
+          while (test > -1 && arr[test] < value) {
+            arr[test + 1] = arr[test]
+            test = test - 1
+          }
+        }
+        else {
+          while (test > -1 && arr[test] > value) {
+            arr[test + 1] = arr[test]
+            test = test - 1
+          }
         }
         arr[test + 1] = value
       }
@@ -39,14 +49,23 @@ export const sort = {
     },
 
     // the same insertion sort, by sub-property "name" this time
-    SortByAttribute (arr, attribute) {
+    SortByAttribute (arr, attribute, reverse) {
+      reverse = reverse || false
       console.log(arr)
       for (var i = 0; i < arr.length; i++) {
         var value = arr[i][attribute]
         var test = i - 1
-        while (test > -1 && arr[test][attribute] > value) {
-          arr[test + 1][attribute] = arr[test][attribute]
-          test -= 1
+        if (reverse) {
+          while (test > -1 && arr[test][attribute] < value) {
+            arr[test + 1][attribute] = arr[test][attribute]
+            test -= 1
+          }
+        }
+        else {
+          while (test > -1 && arr[test][attribute] > value) {
+            arr[test + 1][attribute] = arr[test][attribute]
+            test -= 1
+          }
         }
         arr[test + 1][attribute] = value
       }
